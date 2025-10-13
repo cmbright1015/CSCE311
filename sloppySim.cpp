50fdc682ea5ebab43caa4314002b2c2d0250737e
Caleb Bright <143642889+cmbright1015@users.noreply.github.com>
2025-10-13 19:45:44 -0400

    sloppySim assignment

--- sloppySim.cpp
+++ sloppySim.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+// All shared data that is used by threads
 struct Shared_Data {
     atomic<long long> global_counter{0};
     vector<long long> local_buckets;
@@ -22,21 +23,29 @@ struct Shared_Data {
     mutex global_mutex;
 };
 
+// estimated CPU work time
 const long long ms_increment = 550000;
 
+
 void cpu_work(int work_time_ms) {
+    // makes a random duration for requested time
     int time_ms = rand() % static_cast<int>(work_time_ms) + (work_time_ms / 2);
     long long increments = static_cast<long long>(time_ms) * ms_increment;
+    // keeps the CPU busy
     volatile long long dummy = 0;
     for (long long i = 0; i < increments; ++i)
         dummy += i;
 }
 
+// sleeps the thread for random amount of time
 void io_work(int work_time_ms) {
     int time_ms = rand() % static_cast<int>(work_time_ms) + (work_time_ms / 2);
     this_thread::sleep_for(chrono::milliseconds(time_ms));
 }
 
+// Each thread does work and increments a local_bucket
+// When the local_bucket gets to be > sloppiness, it sends
+// the count to the global counter and holds the mutex.
 void thread_func(int thread_index, Shared_Data* shared) {
     long long local_bucket = 0;
     for (int i = 0; i < shared->work_iterations; ++i) {
@@ -45,15 +54,17 @@ void thread_func(int thread_index, Shared_Data* shared) {
         else
             io_work(shared->work_time);
         
+        // increment local_bucket after work
         local_bucket++;
 
+        // when the bucket fills up, send to global counter
         if (local_bucket >= shared->sloppiness) {
             lock_guard<mutex> lock(shared->global_mutex);
             shared->global_counter += local_bucket;
             local_bucket = 0;
         }
     }
-
+    // anything leftover also gets sent to global counter
     if (local_bucket > 0)
         lock_guard<mutex> lock(shared->global_mutex);
         shared->global_counter += local_bucket;
@@ -65,6 +76,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // arguments with default values
     int N_Threads = (argc > 1) ? stoi(argv[1]) : 2;
     int sloppiness = (argc > 2) ? stoi(argv[2]) : 10;
     int work_time = (argc > 3) ? stoi(argv[3]) : 10;
@@ -80,6 +92,7 @@ int main(int argc, char* argv[]) {
     shared.do_logging = do_logging;
     shared.local_buckets.resize(N_Threads, 0);
 
+    // print out each argument if logging is enabled
     if (do_logging) {
         cout << "Threads: " << N_Threads << "\n";
         cout << "Sloppiness: " << sloppiness << "\n";
@@ -91,11 +104,12 @@ int main(int argc, char* argv[]) {
 
     auto start = chrono::high_resolution_clock::now();
 
+    // threads
     vector<thread> threads;
     for (int i = 0; i < N_Threads; ++i) {
         threads.emplace_back(thread_func, i, &shared);
     }
-
+    // display global counters value if logging is enabled
     if (do_logging) {
         int log = max(1, work_time * work_iterations / 10);
         for (int i = 0; i < log; ++i) {
@@ -103,7 +117,7 @@ int main(int argc, char* argv[]) {
             cout << "Log time=" << i *10 << "ms, Global time= " << shared.global_counter.load() << endl;
         }
     }
-
+    // when all threads are finished, join them
     for (auto& t : threads)
         t.join();
     
